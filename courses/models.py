ac5604517c4b95b81335f4fa01bc11abb6e1288b
Arash Askari <arash.ask.94@gmail.com>
2023-01-24 21:50:46 +0330

    add custom order field to the model

--- courses/models.py
+++ courses/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-
+from .fields import OrderField
 class Subject(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200 , unique=True)
@@ -31,7 +31,7 @@ class Module(models.Model):
     course = models.ForeignKey(Course , related_name='modules' , on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
-
+    order = OrderField(blank=True , for_fields=['course'])
     def __str__(self):
         return self.title
 
